0287275f8659c2b2b94a6862f6830bd392a0e500
gilbert6555tw <6555tw.gilbert@gmail.com>
2019-11-26 01:08:25 +0800

    補註解

--- Q066.java
+++ Q066.java
@@ -25,20 +25,29 @@ public class Q066 {
 //输入: [1,2,3]
 //输出: [1,2,4]
 //解释: 输入数组表示数字 123。
-        int[] digits = {1, 2, 3};
+        int[] digits = {1, 1, 9};
         int[] digits2 = new Q066().plusOne(digits);
         System.out.println("digits2="+Arrays.toString(digits2));
     }
 
     public int[] plusOne(int[] digits) {
+        //由右往前判斷是否進位
+        //數值+1, 若數值%10!=.0則回傳;否則繼續判斷下一位數
+        
         int length = digits.length;
         for (int i = length - 1; i >= 0; i--) {
+            //數值+1
             digits[i]++;
+            //數值取餘數
             digits[i] = digits[i] % 10;
+            //若數值!=0則回傳否則繼續
             if (digits[i] != 0) {
                 return digits;
+            }else{
+                continue;
             }
         }
+        //若迴圈跑完後未回傳, 則數組長度+1, digits[0] = 1後回傳
         digits = new int[length + 1];
         digits[0] = 1;
         return digits;
